f784f5391e63cb5ab451474e30c27aa0b6327aaa
Pasechna Katerina <pasech.kate@gmail.com>
2023-11-30 15:44:28 +0200

    Database schema has been changed.

--- src/main/resources/db/migration/V1__init_DB.sql
+++ src/main/resources/db/migration/V1__init_DB.sql
@@ -1,19 +1,19 @@
 CREATE TABLE "groups"(
-    id SERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     name VARCHAR(5) NOT NULL
 );
 CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     data_type VARCHAR(50) NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     login VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
     group_id BIGINT,
-    FOREIGN KEY (group_id) REFERENCES "groups" (id) ON DELETE CASCADE
+    FOREIGN KEY (group_id) REFERENCES "groups" (id) ON DELETE SET NULL
 );
 CREATE TABLE courses(
-    id SERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     name VARCHAR(30)
 );
 CREATE TABLE teachers_courses(
@@ -29,7 +29,7 @@ CREATE TABLE group_courses(
     FOREIGN KEY (course_id) REFERENCES "courses" (id) ON DELETE CASCADE
 );
 CREATE TABLE class(
-    id SERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     day VARCHAR(20),
     time BIGINT,
     group_id BIGINT,
@@ -37,7 +37,7 @@ CREATE TABLE class(
     teacher_id BIGINT,
     FOREIGN KEY (group_id) REFERENCES "groups" (id) ON DELETE CASCADE,
     FOREIGN KEY (course_id) REFERENCES "courses" (id) ON DELETE CASCADE,
-    FOREIGN KEY (teacher_id) REFERENCES "users" (id) ON DELETE CASCADE
+    FOREIGN KEY (teacher_id) REFERENCES "users" (id) ON DELETE SET NULL
 );
 CREATE SEQUENCE groups_sequence increment 100 owned by groups.id;
 CREATE SEQUENCE users_sequence increment 100 owned by users.id;
