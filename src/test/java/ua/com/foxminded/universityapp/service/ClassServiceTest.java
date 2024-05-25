fb025115ac3b122c2815d40f708ec19ab1a1fced
Pasechna Katerina <pasech.kate@gmail.com>
2024-05-25 13:17:34 +0300

    tmp

--- src/test/java/ua/com/foxminded/universityapp/service/ClassServiceTest.java
+++ src/test/java/ua/com/foxminded/universityapp/service/ClassServiceTest.java
@@ -7,11 +7,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import ua.com.foxminded.universityapp.TestConfig;
 import ua.com.foxminded.universityapp.model.entity.*;
 import ua.com.foxminded.universityapp.model.entity.Class;
@@ -21,32 +16,18 @@ import ua.com.foxminded.universityapp.repository.GroupRepository;
 import ua.com.foxminded.universityapp.repository.UserRepository;
 import ua.com.foxminded.universityapp.service.impl.ClassGenerator;
 import ua.com.foxminded.universityapp.service.impl.CourseGenerator;
-import ua.com.foxminded.universityapp.service.impl.GroupGenerator;
 import ua.com.foxminded.universityapp.service.impl.UsersGenerator;
 
 import java.time.DayOfWeek;
 import java.util.List;
-import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 @ContextConfiguration(classes = TestConfig.class)
-//@Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @MockBean({ClassGenerator.class, CourseGenerator.class, UsersGenerator.class})
 public class ClassServiceTest {
-
-//    @Container
-//    private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest");
-//
-//    @DynamicPropertySource
-//    static void configureProperties(DynamicPropertyRegistry registry) {
-//        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-//        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-//        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-//
-//    }
 
     @Autowired
     private ClassRepository classRepository;
